634459989ee940182845481d77b04b3f7ce96394
Robert S Lee <sangkyulee@gmail.com>
2022-01-25 15:33:52 -0500

    another example

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-julia  --project --threads auto -O1 hello.jl $@    
+# raw argment list
+echo "Arguments"
+echo "$@"
+
+# pop the first argment as the scripe name
+script="$1"
+shift
+echo "Arguments"
+echo "$@"
+
+# run
+echo julia  --project --threads auto -O1 $script $@    
+julia  --project --threads auto -O1 $script $@    
---
+++ run2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+julia  --project --threads auto -O1 hello.jl $@    
